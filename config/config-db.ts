85ee97e900ed6c0bd04f350dd5e29a1a15301759
Marc Hess <marc@marchess.dev>
2023-07-31 15:55:49 -0400

    testing a change

--- config/config-db.ts
+++ config/config-db.ts
@@ -12,13 +12,18 @@ async function main() {
 
 	try {
 		const db = await pool.connect();
-		await db.query("DROP EXTENSION timescaledb");
-		await db.query("ALTER SYSTEM SET wal_level = logical");
-		await db.query("ALTER SYSTEM SET max_replication_slots = 20");
-		await db.query("ALTER SYSTEM SET wal_keep_size = 2048");
-		await db.query("SELECT pg_reload_conf()");
-
-		console.log("All done please restart the database");
+		const wal = await db.query("SHOW wal_level");
+		console.log(wal.rows[0]);
+		if (wal.rows[0] != "logical") {
+			await db.query("DROP EXTENSION timescaledb");
+			await db.query("ALTER SYSTEM SET wal_level = logical");
+			await db.query("ALTER SYSTEM SET max_replication_slots = 20");
+			await db.query("ALTER SYSTEM SET wal_keep_size = 2048");
+			await db.query("SELECT pg_reload_conf()");
+			console.log("All done please restart the database");
+			return;
+		}
+		console.log("DB is already configured");
 	} catch (err) {
 		console.log(err);
 	}
---
+++ index.ts
@@ -0,0 +1,21 @@
+// prisma.ts
+import "dotenv/config";
+import { PrismaClient } from "@prisma/client";
+import { withPulse } from "@prisma/extension-pulse";
+
+const apiKey: string =
+	process.env.PULSE_API_KEY !== undefined ? process.env.PULSE_API_KEY : "";
+const prisma = new PrismaClient().$extends(withPulse({ apiKey: apiKey }));
+
+async function main() {
+	const subscription = await prisma.user.subscribe();
+
+	if (subscription instanceof Error) {
+		throw subscription;
+	}
+
+	for await (const event of subscription) {
+		console.log("just received an event:", event);
+	}
+}
+main();
